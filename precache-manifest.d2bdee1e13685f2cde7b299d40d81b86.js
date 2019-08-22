b3911568499ec6e906544318d47ba37fe96b8d71
eladsegal <eladsegal@users.noreply.github.com>
2019-08-22 16:54:08 +0300

    Updates

---
+++ precache-manifest.d2bdee1e13685f2cde7b299d40d81b86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea612f2389f8f2cd36cb5e68f9fd5cef",
+    "url": "/DROP-explorer/index.html"
+  },
+  {
+    "revision": "26cae076d995e257a5b6",
+    "url": "/DROP-explorer/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "153c47640ff393e37fd8",
+    "url": "/DROP-explorer/static/css/main.5c4c39e8.chunk.css"
+  },
+  {
+    "revision": "26cae076d995e257a5b6",
+    "url": "/DROP-explorer/static/js/2.dda4526f.chunk.js"
+  },
+  {
+    "revision": "153c47640ff393e37fd8",
+    "url": "/DROP-explorer/static/js/main.ca2604b0.chunk.js"
+  },
+  {
+    "revision": "4900d8203044a2c3c9d6",
+    "url": "/DROP-explorer/static/js/runtime~main.063dbb95.js"
+  }
+]);
